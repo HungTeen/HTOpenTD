0e2d227328418642dcd1c4378709f3ae5ef7ad2a
HungTeen <1687289734@qq.com>
2022-12-14 16:48:25 +0800

    新建文件夹

---
+++ src/main/java/hungteen/opentd/OpenTD.java
@@ -0,0 +1,18 @@
+package hungteen.opentd;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @program: HTOpenTD
+ * @author: HungTeen
+ * @create: 2022-12-14 12:41
+ **/
+@Mod(OpenTD.MOD_ID)
+public class OpenTD {
+
+    public static final String MOD_ID = "opentd";
+
+    public OpenTD() {
+
+    }
+}
