e0d786ff07c3cdef89b07ad10a3c6047b0721f61
HungTeen <1687289734@qq.com>
2023-05-17 23:35:25 +0800

    完成激光组件（大概）

--- src/main/java/hungteen/opentd/common/codec/LaserGoalSetting.java
+++ src/main/java/hungteen/opentd/common/codec/LaserGoalSetting.java
@@ -3,7 +3,10 @@ package hungteen.opentd.common.codec;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import hungteen.opentd.api.interfaces.IEffectComponent;
+import hungteen.opentd.api.interfaces.ITargetFilter;
 import hungteen.opentd.common.impl.effect.HTEffectComponents;
+import hungteen.opentd.common.impl.filter.HTTargetFilters;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
 import java.util.List;
@@ -14,18 +17,20 @@ import java.util.Optional;
  * @program HTOpenTD
  * @data 2023/5/15 18:59
  */
-public record LaserGoalSetting(int coolDown, int duration, int effectInterval, IEffectComponent continueEffect, IEffectComponent finalEffect, double laserDistance, boolean needRest, boolean canBlockByEntity, Optional<SoundEvent> attackSound) {
+public record LaserGoalSetting(int coolDown, int duration, int effectInterval, ITargetFilter laserFilter, IEffectComponent continueEffect, IEffectComponent finalEffect, float laserWidth, double trackDistance, double laserDistance, boolean needRest, Optional<ResourceLocation> laserTexture) {
 
     public static final Codec<LaserGoalSetting> CODEC = RecordCodecBuilder.<LaserGoalSetting>mapCodec(instance -> instance.group(
-            Codec.intRange(1, Integer.MAX_VALUE).optionalFieldOf("cool_down", 30).forGetter(LaserGoalSetting::coolDown),
-            Codec.intRange(0, Integer.MAX_VALUE).optionalFieldOf("duration", 0).forGetter(LaserGoalSetting::duration),
+            Codec.intRange(1, Integer.MAX_VALUE).optionalFieldOf("cool_down", 100).forGetter(LaserGoalSetting::coolDown),
+            Codec.intRange(0, Integer.MAX_VALUE).optionalFieldOf("duration", 80).forGetter(LaserGoalSetting::duration),
             Codec.intRange(1, Integer.MAX_VALUE).optionalFieldOf("effect_interval", 1).forGetter(LaserGoalSetting::effectInterval),
+            HTTargetFilters.getCodec().fieldOf("laser_filter").forGetter(LaserGoalSetting::laserFilter),
             HTEffectComponents.getCodec().fieldOf("continue_effect").forGetter(LaserGoalSetting::continueEffect),
             HTEffectComponents.getCodec().fieldOf("final_effect").forGetter(LaserGoalSetting::finalEffect),
+            Codec.floatRange(0, Float.MAX_VALUE).optionalFieldOf("laser_width", 0.5F).forGetter(LaserGoalSetting::laserWidth),
+            Codec.doubleRange(0, Double.MAX_VALUE).optionalFieldOf("track_distance", 10D).forGetter(LaserGoalSetting::trackDistance),
             Codec.doubleRange(0, Double.MAX_VALUE).optionalFieldOf("laser_distance", 15D).forGetter(LaserGoalSetting::laserDistance),
             Codec.BOOL.optionalFieldOf("need_rest", false).forGetter(LaserGoalSetting::needRest),
-            Codec.BOOL.optionalFieldOf("can_block_by_entity", false).forGetter(LaserGoalSetting::canBlockByEntity),
-            Codec.optionalField("attack_sound", SoundEvent.CODEC).forGetter(LaserGoalSetting::attackSound)
+            Codec.optionalField("laser_texture", ResourceLocation.CODEC).forGetter(LaserGoalSetting::laserTexture)
     ).apply(instance, LaserGoalSetting::new)).codec();
 
 }
--- src/main/java/hungteen/opentd/common/entity/ai/TowerLaserAttackGoal.java
+++ src/main/java/hungteen/opentd/common/entity/ai/TowerLaserAttackGoal.java
@@ -1,92 +1,129 @@
 package hungteen.opentd.common.entity.ai;
 
-import hungteen.opentd.common.codec.AttackGoalSetting;
+import hungteen.htlib.util.helper.registry.EntityHelper;
+import hungteen.opentd.api.interfaces.IEffectComponent;
 import hungteen.opentd.common.codec.LaserGoalSetting;
 import hungteen.opentd.common.entity.TowerEntity;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.Difficulty;
-import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.monster.ElderGuardian;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.EnumSet;
+import java.util.Objects;
 
 /**
  * @author PangTeen
  * @program HTOpenTD
  * @data 2023/5/15 19:23
  */
-public class TowerLaserAttackGoal extends HTGoal{
+public class TowerLaserAttackGoal extends HTGoal {
 
     private final TowerEntity towerEntity;
+    protected LivingEntity target;
     private int attackTime;
 
     public TowerLaserAttackGoal(TowerEntity towerEntity) {
         this.towerEntity = towerEntity;
         this.setFlags(EnumSet.of(Goal.Flag.LOOK));
+        this.attackTime = -1;
     }
 
     @Override
     public boolean canUse() {
-        LivingEntity livingentity = this.towerEntity.getTarget();
-        return setting() != null && livingentity != null && livingentity.isAlive();
+        if (this.setting() == null) {
+            return false;
+        }
+        this.target = this.towerEntity.getTarget();
+        if (!this.checkTarget()) {//can not attack because of its target(such as height limit).
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean canContinueToUse() {
-        return setting() != null &&  this.towerEntity.getTarget() != null;
-    }
-
-    @Override
-    public void start() {
-        this.attackTime = -1;
-        LivingEntity livingentity = this.towerEntity.getTarget();
-        if (livingentity != null) {
-            this.towerEntity.getLookControl().setLookAt(livingentity, 90.0F, 90.0F);
-        }
-
-        this.towerEntity.hasImpulse = true;
+        return this.canUse();
     }
 
     @Override
     public void stop() {
         this.towerEntity.setActiveAttackTarget(0);
-        this.towerEntity.setTarget(null);
+        this.attackTime = -1;
     }
 
     @Override
     public void tick() {
-        LivingEntity livingentity = this.towerEntity.getTarget();
-        if (livingentity.level instanceof ServerLevel serverLevel && livingentity != null) {
-            this.towerEntity.getLookControl().setLookAt(livingentity, 90.0F, 90.0F);
-            if (!this.towerEntity.hasLineOfSight(livingentity)) {
-                this.towerEntity.setTarget(null);
-            } else {
-                ++ this.attackTime;
-                if (this.attackTime == 0) {
-                    this.towerEntity.setActiveAttackTarget(livingentity.getId());
-                    if (!this.towerEntity.isSilent()) {
-                        this.towerEntity.level.broadcastEntityEvent(this.towerEntity, (byte)21);
-                    }
-                } else {
-                    if(this.attackTime % this.setting().effectInterval() == 0){
-                        this.setting().continueEffect().effectTo(serverLevel, towerEntity, livingentity, livingentity.blockPosition());
-                    }
-                    if (this.attackTime >= this.setting().duration()) {
-                        this.setting().finalEffect().effectTo(serverLevel, towerEntity, livingentity, livingentity.blockPosition());
-                        this.towerEntity.setTarget(null);
-                    }
+        if (this.towerEntity.canChangeDirection()) {
+            this.towerEntity.getLookControl().setLookAt(this.target, 90.0F, 90.0F);
+        }
+        // 冷却结束 或者 动画已经开始。
+        if (this.towerEntity.preLaserTick <= 0 || this.attackTime >= 0) {
+            ++this.attackTime;
+            if (this.attackTime == 0) {
+                this.towerEntity.setActiveAttackTarget(this.target.getId());
+                if (!this.towerEntity.isSilent()) {
+                    this.towerEntity.level.broadcastEntityEvent(this.towerEntity, (byte) 21);
                 }
-
-                super.tick();
             }
+            if (this.attackTime > Objects.requireNonNull(this.setting()).duration()) {
+                this.attackTime = -1;
+                this.towerEntity.setActiveAttackTarget(0);
+                this.towerEntity.preAttackTick = Objects.requireNonNull(this.setting()).coolDown();
+            } else {
+                if (this.attackTime > 0 && this.attackTime % this.setting().effectInterval() == 0) {
+                    effectTo(this.setting().continueEffect());
+                }
+                if (this.attackTime >= this.setting().duration()) {
+                    effectTo(this.setting().finalEffect());
+                }
+            }
+        } else {
+            --this.towerEntity.preLaserTick;
+        }
+        if (Objects.requireNonNull(this.setting()).needRest()) {
+            this.towerEntity.setResting(this.towerEntity.preLaserTick > 0);
         }
     }
 
-    private LaserGoalSetting setting(){
+    private void effectTo(IEffectComponent effect) {
+        if(this.towerEntity.level instanceof ServerLevel serverLevel){
+            final Vec3 st = this.towerEntity.getEyePosition();
+            final Vec3 ep = this.target.getEyePosition();
+            final Vec3 laser = ep.subtract(st);
+            final AABB aabb = EntityHelper.getEntityAABB(this.towerEntity, this.setting().laserDistance(), this.setting().laserDistance());
+            EntityHelper.getPredicateEntities(this.towerEntity, aabb, Entity.class, entity -> {
+                if(this.setting().laserFilter().match(serverLevel, this.towerEntity, entity)){
+                    final Vec3 tp = entity.getEyePosition();
+                    final Vec3 vec = tp.subtract(st);
+                    if(vec.length() < this.setting().laserDistance() && vec.length() > 0 && laser.length() > 0) {
+                        final double cos = laser.dot(vec) / vec.length() / laser.length();
+                        final double sin = Math.sqrt(1 - cos * cos);
+                        return sin * vec.length() <= this.setting().laserWidth();
+                    }
+                }
+                return false;
+            }).forEach(entity -> {
+                effect.effectTo(serverLevel, this.towerEntity, entity);
+            });
+            effect.effectTo(serverLevel, this.towerEntity, this.target.blockPosition());
+        }
+    }
+
+    private LaserGoalSetting setting() {
         return this.towerEntity.getLaserSetting();
+    }
+
+    protected boolean checkTarget() {
+        return EntityHelper.isEntityValid(this.target)
+                && this.towerEntity.canAttack(this.target)
+                && this.towerEntity.getSensing().hasLineOfSight(this.target)
+                && this.towerEntity.distanceTo(this.target) <= Math.min(
+                Objects.requireNonNull(this.setting()).laserDistance(),
+                Objects.requireNonNull(this.setting()).trackDistance()
+        );
     }
 
 
